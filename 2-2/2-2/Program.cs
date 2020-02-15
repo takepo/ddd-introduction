5b4425d1a396df4413b37b31c9659ece1ce7ae76
YokogoshiNahoko <takeshima.nahoko+1@gmail.com>
2020-02-15 10:53:44 +0900

    feat: add 2-2

---
+++ 2-2/2-2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Application
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var fullName = "naruse masanobu";
+            var tokens = fullName.Split(' ');
+            var lastname = tokens[0];
+            Console.WriteLine(lastname);
+        }
+    }
+}
