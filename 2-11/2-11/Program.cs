93324edfa1593ac0d3bf1d55e94e243b83020f72
YokogoshiNahoko <takeshima.nahoko+1@gmail.com>
2020-02-15 18:05:18 +0900

    feat: add 2-11

---
+++ 2-11/2-11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Application
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // 数値の変更
+            var num = 0;
+            num = 1;
+
+            //文字の変更
+            var c = '0';
+            c = 'b';
+
+            // 文字列の変更
+            var greet = "こんにちは";
+            greet = "hello";
+        }
+    }
+}
