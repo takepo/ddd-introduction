8625403e1d0d92cd3bbe6d2009710884ba9052bc
YokogoshiNahoko <takeshima.nahoko+1@gmail.com>
2020-02-15 17:10:47 +0900

    feat: add 2-7

---
+++ 2-7/2-7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Application
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var greet = "こんにちは";
+            Console.WriteLine(greet);
+
+            greet = "Hello";
+            Console.WriteLine(greet);
+        }
+    }
+}
